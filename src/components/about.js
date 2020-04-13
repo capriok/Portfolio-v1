6dc1029be69cd1514ba0893ee3d9a86ed57edc29
Kyle Caprio <capriok7@yahoo.com>
2020-04-12 20:15:04 -0700

    about me page

--- src/components/about.js
+++ src/components/about.js
@@ -13,41 +13,29 @@ export default function AboutRoute() {
           </div>
           <div className="body" style={theTheme.whiteFont}>
             <h1>—— About Me ——</h1>
-            <p style={{ textIndent: '15px' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. autem
-              ipsam voluptatum? Dolorum eaque,earum rem autem numquam pariatur
-              dolor sit amet consectetur adipisicing elit Omnis
-              placeat adipisicing elit. Dolor sit amet consectetur adipisicing
-              elit Error quaerat similique amet sed minima?
-            </p>
-            <br></br>
-            <p style={{ textIndent: '15px' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. autem
-              ipsam voluptatum?  Dolorum rem autem numquam pariatur
-              dolor sit amet consectetur adipisicing elit Omnis minusadipisicing
-              placeat dolor adipisicing elit Omnis minusconsecteturadipisicing
-              placeat adipisicing elit. dolor sit amet consectetur adipisicing
-              elit Error quaerat similique amet sed minima?
-            </p>
-            <br></br>
-            <p style={{ textIndent: '15px' }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. autem
-              ipsam. Dolor sit amet consectetur adipisicing
-              elit Error quaerat similique amet sed minima?
+            <p className="indent">
+              My name is Kyle Caprio. I'm a self taught software engineer based in Tempe, Arizona.
+              I found my passion for learning computer sciences at the age of 16 and have been programming ever since.
+              Despite not attending college, I've dedicated countless hours to learning and expanding my passion.
+              Being able to effectively teach myself is something I am most proud of about myself.
+              I have developed an avid work ethic and disciplinary skills along my journey in life.
+              Strong creative and critical thinking skills have helped me to excel in areas
+              like web development as well as functional and object oriented programming.
+              In free my time, I immerse myself in learning new programming technologies and how to better myself as a programmer
+              but also find joy with meditaion, music, hiking, and spending time under the sun.
             </p>
             <h1>—— Technical Skills ——</h1>
             <ul>
-              <li><p>Lorem alida sit amet consectetur adipisicing elit.</p></li>
-              <li><p>Lorem dolor amet consectetur ipsu madipisicing elit. Sit adipisicing elit.</p></li>
-              <li><p>Lorem ipsum dolor sit amet consectetur elit.</p></li>
-              <li><p>Lorem amet consectetur adipisicing elit.</p></li>
+              <li><p>HTML, CSS, SASS,  (Front End Development)</p></li>
+              <li><p>React.js, Node.js, Express (Javascript)</p></li>
+              <li><p>Relational databases: GraphQl, SQL</p></li>
+              <li><p>Non-Relational databases: MongoDB</p></li>
+              <li><p>Library use: Gatsby, NEXT.js, Material-UI</p></li>
             </ul>
-            <h1>—— Experience ——</h1>
+            <h1>—— Proffesional Experience ——</h1>
             <ul>
-              <li><p>Lorem ipsum dolor sit amet consectetur elit.</p></li>
-              <li><p>Lorem ahula sit amet elit.</p></li>
-              <li><p>Lorem alida sit consectetur amet adipisicing elit.</p></li>
-              <li><p>Lorem dolor amet elit. Lorem ipsum consectetur adipisicing dolor sit adipisicing elit.</p></li>
+              <li><p>Freelance website design and deployment projects for 2 small business clients</p></li>
+              <li><p>&lt; 1 year consulting on an open source user authentication application</p></li>
             </ul>
           </div>
         </div>
