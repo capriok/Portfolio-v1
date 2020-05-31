dd096f89f068a3a82b5aa25be677a882d192cc24
Kyle Caprio <capriok7@yahoo.com>
2020-05-30 17:25:47 -0700

    resume mobile

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,19 +1,22 @@
 import React, { useEffect } from 'react'
 import resume from '../documents/Kyle-Caprio-Resume.pdf'
-
+import ResumeJPG from '../gallery/resume.jpg'
 const Resume = () => {
-
+  const isMobile = window.innerWidth > 500
   useEffect(() => { document.title = `Portfolio | Resume` }, [])
   return (
     <>
-      <div className="resume">
-        <object
-          data={resume}
-          type="application/pdf"
-          width="100%"
-          height="100%"
-        />
-      </div>
+      {isMobile
+        ? <div className="resume">
+          <object
+            data={resume}
+            type="application/pdf"
+            width="100%"
+            height="100%"
+          />
+        </div>
+        : <img className="resume-mobile" src={ResumeJPG} alt="" />
+      }
     </>
   )
 }
