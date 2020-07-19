4e3d4af0cdcd108a5407cb7d70a434f969c444c6
Kyle Caprio <kyle@MacBook-Pro.local>
2020-07-19 12:27:23 -0700

    blog progress

--- src/components/routes/blog.js
+++ src/components/routes/blog.js
@@ -1,20 +1,28 @@
-import React, { lazy, Suspense } from 'react'
-import { importMDX } from 'mdx.macro'
+import React from 'react'
 import BlogPost from '../templates/blogpost'
 
-const Content = lazy(() => importMDX('./posts/test.mdx'))
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 const Blog = () => {
+	const blogs = {
+		blogOne: {
+			title: 'tes blog one',
+			date: '07-17-2020',
+			category: 'tech',
+			route: 'blogone',
+			body: 'this is the body content of test blog one'
+		}
+	}
 
-  return (
-    <div className="blog-posts-layout">
-      <h1 className="header">Blog</h1>
-      <BlogPost />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Content />
-      </Suspense>
-    </div>
-  )
+	return (
+		<div className="blog-posts-layout">
+			<h1 className="header">Blog</h1>
+			{blogs.map((blog) => (
+				<Route path={`blog/${blog.route}`} render={() => blog.component} />
+			))}
+		</div>
+	)
 }
 
 export default Blog
